3aaf022da8d381fac0172c5889c11f1049500e89
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-06 23:48:35 +0300

    Create analyses.js

---
+++ routes/analyses.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const analysisController = require('../controllers/analysisController');
+const { authenticateToken } = require('../middleware/auth');
+const { validateAnalysis } = require('../middleware/validation');
+
+// All routes require authentication
+router.use(authenticateToken);
+
+// POST /analyses/voice
+router.post('/voice', analysisController.submitVoiceAnalysis);
+
+// POST /analyses/facial
+router.post('/facial', analysisController.submitFacialAnalysis);
+
+// POST /analyses/test
+router.post('/test', validateAnalysis, analysisController.submitTestAnalysis);
+
+// GET /analyses/user
+router.get('/user', analysisController.getUserAnalyses);
+
+module.exports = router; 
