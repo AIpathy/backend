1f1ecf285a1eb1e183c6a7761b9b8c9c497d7397
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-17 16:35:46 +0300

    update database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'aipathy_db',
+  database: process.env.DB_NAME || 'db_aipathy',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
